7d589f176d1e19c4361d63eb5d37c8cd7910a80f
Mohamad_OC <61678244+mohamadOC@users.noreply.github.com>
2022-07-22 23:25:03 +0200

    Update FixSprint.php

--- src/MohamadOC/FixSprint.php
+++ src/MohamadOC/FixSprint.php
@@ -74,7 +74,7 @@ class FixSprint extends PluginBase implements Listener{
     /*
     task to check if the player is moving!, if not then stop the sprint.
     */
-    $this->getScheduler()->scheduleRepeatingTask(new Movecheck(), 1);
+    $this->getScheduler()->scheduleRepeatingTask(new Movecheck(), 5);
     self::$instance = $this;
 
 	}
